53a29bb95a5f6581012ab6685cdaa62eb3b2ce6e
Juanjo Morell <34169360+JuanjoMorell@users.noreply.github.com>
2021-09-09 12:46:48 +0200

    CROS Permission

--- CitasApi/Startup.cs
+++ CitasApi/Startup.cs
@@ -59,6 +59,12 @@ namespace CitasApi
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseCors(opt =>
+            {
+                opt.WithOrigins("http://localhost:4200");
+                opt.AllowAnyMethod();
+                opt.AllowAnyHeader();
+            });
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
